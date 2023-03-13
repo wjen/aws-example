524dcbb110bda58cc49a8543d497e043ef7c94ea
wen jen <wjen1214@gmail.com>
2023-03-13 15:47:35 -0700

    add app

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hellloooo there');
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log('app started wennnnn');
+});
